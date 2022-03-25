1fa2761736c2bce5074512320232155d70ae60ce
Claudio Cambra <claudio.cambra@gmail.com>
2022-03-25 19:55:38 +0000

    Revert Package.resolved

--- KDE Connect/KDE Connect/Swift Backend/Backend.swift
+++ KDE Connect/KDE Connect/Swift Backend/Backend.swift
@@ -41,17 +41,17 @@ let backgroundService: BackgroundService = BackgroundService(connectedDeviceView
 extension UIImpactFeedbackGenerator.FeedbackStyle : CaseIterable {
     public var text: Text {
         switch self {
-        case .light: return Text("Light", comment:"Light haptic feedback level")
-        case .medium: return Text("Medium", comment:"Medium haptic feedback level")
-        case .heavy: return Text("Heavy", comment:"Hard haptic feedback level")
-        case .rigid: return Text("Rigid", comment:"Rigid haptic feedback level")
-        case .soft: return Text("Soft", comment:"Soft haptic feedback level")
-        @unknown default: return Text("Other", comment: "Unknown feedback level 🤷‍♂️")
+        case .light: return Text("Light", comment: "Light haptic feedback level")
+        case .medium: return Text("Medium", comment: "Medium haptic feedback level")
+        case .heavy: return Text("Heavy", comment: "Hard haptic feedback level")
+        case .soft: return Text("Soft", comment: "Soft haptic feedback level")
+        case .rigid: return Text("Rigid", comment: "Rigid haptic feedback level")
+        @unknown default: return Text("Other", comment: "Unknown haptic feedback level")
         }
     }
     
     public static var allCases: [UIImpactFeedbackGenerator.FeedbackStyle] {
-        return [.light, .medium, .heavy, .rigid, .soft]
+        return [.light, .medium, .heavy, .soft, .rigid]
     }
 }
 
